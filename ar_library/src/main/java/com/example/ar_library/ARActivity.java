63fe5028a68fa678db2f39df77d8d63c26f64b1a
Prince Abhinav Krishnamurti <kprince@findabilitysciences.com>
2019-05-23 14:25:30 +0530

    toast added

--- ar_library/src/main/java/com/example/ar_library/ARActivity.java
+++ ar_library/src/main/java/com/example/ar_library/ARActivity.java
@@ -1,8 +1,11 @@
 package com.example.ar_library;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
@@ -142,5 +145,9 @@ public class ARActivity  {
             viewRenderableList = new ArrayList<>();
             anchorList = new ArrayList<>();
         }
+    }
+    public static void s(Context c, String message)
+    {
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
     }
 }
